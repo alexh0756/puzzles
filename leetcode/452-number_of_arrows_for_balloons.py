717a8f5e04fc41cded323e9c47d751d4d7612f0c
alex <alex.scheduling@gmail.com>
2024-05-05 20:32:45 +1000

    leetcode arrows for balloons

--- leetcode/452-number_of_arrows_for_balloons.py
+++ leetcode/452-number_of_arrows_for_balloons.py
@@ -1,40 +1,16 @@
 def findMinArrowShots(points):
 
-    points.sort()
-    map_nums = {-1: []}
-
-    i = 0
-    while i < len(points):
-        if len(points) == 1:
-            map_nums[-1].append(points[i])
-            points.pop(i)
-        elif points[i][1] < points[i+1][0]:
-            map_nums[-1].append(points[i])
-            points.pop(i)
+    arrows, lower_bound = 0, -2**33
+    for r_min, r_max in sorted(points):
+        if lower_bound < r_min:
+            lower_bound = r_max
+            arrows += 1
         else:
-            for val in range(points[i+1][0], points[i][1]+1):
-                if val not in map_nums:
-                    map_nums.update({val: [points[i], points[i+1]]})
-                else:
-                    for rge in [points[i], points[i+1]]:
-                        if rge in map_nums[i]:
-                            continue
-                        else:
-                            map_nums[i].append(rge)
-            points.pop(i)
+            lower_bound = min(lower_bound, r_max)
 
-    unique_combos = {}
-    for k, v in map_nums.items():
-        if not v:
-            continue
-        tup_val = tuple(map(tuple, v))
-        if tup_val not in unique_combos.keys():
-            unique_combos.update({tup_val: {'i': k, 'len': len(v)}})
+    return arrows
 
-    darts = len(unique_combos)
-
-    return darts
-
+print(findMinArrowShots([[1,3],[3,6],[4,6],[5,8],[9,10]]))
 print(findMinArrowShots([[10,16],[2,8],[1,6],[7,12]]))
 print(findMinArrowShots([[1,2],[3,4],[5,6],[7,8]]))
 print(findMinArrowShots([[1,2],[2,3],[3,4],[4,5]]))
