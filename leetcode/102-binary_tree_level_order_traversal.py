191cfb762e47ba32c6b4bec9beb343dcaa46c3b6
alex <alex.scheduling@gmail.com>
2024-05-29 10:24:09 +1000

    binary tree level order traversal

---
+++ leetcode/102-binary_tree_level_order_traversal.py
@@ -0,0 +1,46 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+    def __repr__(self) -> str:
+        return f"tree(val: {self.val}, left: {self.left}, right: {self.right})"
+
+
+def build_tree(root, index):
+    if root[index] == None:
+        return None
+
+    tree = TreeNode(root[index])
+    if 2*index+1 < len(root):
+        tree.left = build_tree(root, 2*index+1)
+    if 2*index+2 < len(root):
+        tree.right = build_tree(root, 2*index+2)
+
+    return tree
+
+
+def averageOfLevels(root):
+
+    if not root:
+        return root
+
+    nodes = [[root]]
+    i = 0
+    while i < len(nodes):
+        for node in nodes[i]:
+            if i + 1 == len(nodes):
+                nodes.append([])
+            if node.left:
+                nodes[i+1].append(node.left)
+            if node.right:
+                nodes[i+1].append(node.right)
+        i += 1
+    vals = [[root.val for root in l] for l in nodes if l]
+
+    return vals
+
+
+root = [3,9,20,None,None,15,7]
+root = build_tree(root, 0)
+print(averageOfLevels(root))
